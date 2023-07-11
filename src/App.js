1c13d1879d22ee922ddb7ec3383bc77308b3521f
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-11 15:44:14 +0300

    Style fix for overflow, and ListItem component for Package list items.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 const App = () => {
   return (
     <div className="app">
@@ -22,7 +27,26 @@ const Form = () => {
 };
 
 const PackingList = () => {
-  return <div className="list">Lists</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <ListItem item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const ListItem = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = () => {

c5d2734ba9d6813aabb2251af3e4a4ee81dc08d0
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-12 14:54:47 +0300

    Removed unnecessary data.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-];
-
 const App = () => {
-  const [items, setItems] = useState([...initialItems]);
+  const [items, setItems] = useState([]);
 
   const handleAddItem = (newItem) => {
     setItems((items) => [...items, newItem]);

c823db2d21353797a386cece3d36acb38501c926
1811501642-Kalam <73571222+kingkhanthekk@users.noreply.github.com>
2023-07-12 14:53:56 +0300

    Added a condition to show start adding item message when there is no item.

--- src/App.js
+++ src/App.js
@@ -121,6 +121,13 @@ const ListItem = ({ item, onDeleteItem, onToggleItem }) => {
 };
 
 const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to the list.</em>
+      </footer>
+    );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage = Math.round((numPacked / numItems) * 100);
